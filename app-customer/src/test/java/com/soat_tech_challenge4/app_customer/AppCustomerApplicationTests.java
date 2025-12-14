f2e3e93068e9abb2fc24e9ab2693d86ae9a2b30d
Thiago Frozzi <thiagofrozzi@hotmail.com>
2025-12-14 19:35:42 -0300

    initial project

---
+++ app-customer/src/test/java/com/soat_tech_challenge4/app_customer/AppCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soat_tech_challenge4.app_customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
